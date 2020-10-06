44ca0acc883ffd8d6c544c09bb9ff8bd98137547
Taylor Phelps <tphelps1993@gmail.com>
2020-10-05 20:50:24 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# William Setstatespear
+
+This is a sample project used for education of React Router.
+
+The code is organised into different branches which have progressively more complexity.
+
+## The branches
+
+### 1. Home Page
+
+This branch adds a route for the home page
+
+```bash
+git checkout 1-homepage
+```
+
+### 2. Poem Page
+
+This branch adds a dynamic route for the poem page
+
+```bash
+git checkout 2-poempage
+```
+
+### 3. Link Instances
+
+This branch swaps all the anchors for Link components
+
+```bash
+git checkout 3-link-instances
+```
---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PoemListPage from './PoemListPage'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <nav>
+          <a href='/'>Poem List</a>
+        </nav>
+        <header>
+          <h1>William Setstatespear</h1>
+        </header>
+        <main>
+          <PoemListPage />
+        </main>
+        <footer>
+          <p>© WilliamSetstatespear, 2018. All Rights Reserved.</p>
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
